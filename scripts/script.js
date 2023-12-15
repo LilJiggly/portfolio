83ab34f62489eadd104c97c733184492eed03e95
LilJiggly <58260324+LilJiggly@users.noreply.github.com>
2023-12-15 17:11:24 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,24 @@
 console.log('ik ben gelinkt')
 
-var buttonLayover = document.getElementById("buttonMenu")
-var menuLau = document.getElementById("menu")
-var pijltje = document.getElementById("pijl")
+// var buttonLayover = document.getElementById("buttonMenu")
+// var menuLau = document.getElementById("menu")
+// var pijltje = document.getElementById("pijl")
 
-buttonLayover.onclick = test;
+// buttonLayover.onclick = test;
 
-            function test() {
-                console.log("joden");
-                menuLau.classList.toggle("active");
-                buttonLayover.classList.toggle("activeButton");
+//             function test() {
+//                 console.log("joden");
+//                 menuLau.classList.toggle("active");
+//                 buttonLayover.classList.toggle("activeButton");
                 
-            };
+//             };
 
 
+var menuButton = document.getElementById("menuButton")
+var menuScreen = document.getElementById("menuScreen")
 
+menuButton.onclick = openMenu
 
-
-            
+function openMenu() {
+    menuScreen.classList.toggle("active");
+}
